0fbd3322963a42ef900462033ea735edc4fe5f15
Ilia Melnik <65551632+MelnikIlia@users.noreply.github.com>
2022-02-14 20:04:12 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,113 +1,114 @@
 /* Elements */
-let body = $("body");
-let header = $(".header");
-let nav = $(".nav");
-let nav_menu = $(".nav__menu");
-let modal_register = $(".modal-register");
-let form_signIn = $(".form-sign-in");
-let form_signUp = $(".form-sign-up");
+const body           = document.body;
+const header         = document.querySelector('.header');
+const nav            = document.querySelector('.nav');
+const nav_menu       = document.querySelector('.nav__menu');
+const modal_register = document.querySelector('.modal-register');
+const form_signIn    = document.querySelector('.form-sign-in');
+const form_signUp    = document.querySelector('.form-sign-up');
 
 /* Buttons */
-let button_burger = $("#button-burger");
-let signIn_button = $("#signIn");
-let signUp_button = $("#signUp");
-let modal_close = $(".modal-close");
+const button_burger  = document.getElementById('button-burger');
+const signIn_button  = document.getElementById('signIn');
+const signUp_button  = document.getElementById('signUp');
+const modal_close    = document.querySelectorAll('.modal-close');
 
 let scroll_position = 0;
 let ticking = false;
 
 function animateHeader(scroll_pos) {
-	if (scroll_pos > 20 && ticking) {
-		header.addClass("scrolled");
-		nav.addClass("scrolled");
-	} else {
-		header.removeClass("scrolled");
-		nav.removeClass("scrolled");
-	}
+  if (scroll_pos > 20 && ticking) {
+    header.classList.add('scrolled');
+    nav.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+    nav.classList.remove('scrolled');
+  }
 }
 
-window.addEventListener("scroll", function () {
-	scroll_position = window.scrollY;
+window.addEventListener('scroll', function () {
+  scroll_position = window.scrollY;
 
-	if (!ticking && window.matchMedia('(min-width: 768px)').matches) {
-		window.requestAnimationFrame(function () {
-			animateHeader(scroll_position);
-			ticking = false;
-		});
-		ticking = true;
-	}
+  if (!ticking && window.matchMedia('(min-width: 768px)').matches) {
+    window.requestAnimationFrame(function () {
+      animateHeader(scroll_position);
+      ticking = false;
+    });
+    ticking = true;
+  }
 });
 
-$(document).ready(() => {
-	$(".testimonials__list").slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		dots: true,
-		arrows: false
-	});
+window.onload = function () {
+  new Swiper('.testimonials', {
+    speed: 500,
+    slidesPerView: 1,
+    pagination: {
+      el: '.testimonials-pagination',
+      type: 'bullets',
+			clickable: true
+    },
+  });
 
-	$(".blog-feed__list").slick({
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		dots: true,
-		arrows: false,
-		responsive: [
-			{
-				breakpoint: 1200,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2,
-					dots: true,
-					arrows: false,
-				}
+	new Swiper('.blog-feed', {
+    slidesPerView: 1,
+		slidesPerGroup: 1,
+    pagination: {
+      el: '.blog-feed-pagination',
+      type: 'bullets',
+			clickable: true,
+			centeredSlides: true
+    },
+		breakpoints: {
+			620: {
+				slidesPerView: 2,
+				slidesPerGroup: 2
 			},
-			{
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
-					dots: true,
-					arrows: false,
-				}
+			1020: {
+        speed: 1000,
+				slidesPerView: 3,
+				slidesPerGroup: 3
 			}
-		]
-	});
-
-	button_burger.click((e) => {
-		e.preventDefault();
-		button_burger.toggleClass("active");
-		nav_menu.toggleClass("active");
-		body.toggleClass("lock");
-	});
-
-	signIn_button.click((e) => {
-		e.preventDefault();
-		modal_register.addClass("showed");
-		form_signIn.toggleClass("showed");
-		
-		if(!body.classList.contains("lock")) {
-			body.toggleClass("lock");
 		}
-	});
+  });
 
-	signUp_button.click((e) => {
-		e.preventDefault();
-		modal_register.addClass("showed");
-		form_signUp.toggleClass("showed");
-		
-		if(!body.classList.contains("lock")) {
-			body.toggleClass("lock");
-		}
-	});
+  button_burger.addEventListener('click', function (e) {
+    e.preventDefault();
+    button_burger.classList.toggle('active');
+    nav_menu.classList.toggle('active');
+    body.classList.toggle('lock');
+  });
 
-	modal_close.click((e) => {
-		e.preventDefault();
-		modal_register.removeClass("showed");
-		form_signIn.removeClass("showed");
-		form_signUp.removeClass("showed");
-		
-		if(!nav_menu.classList.contains("active")) {
-			body.removeClass("lock");
-		}
-	});
-});
+  function showRegistration(elem) {
+    modal_register.classList.add('showed');
+    elem.classList.toggle('showed');
+
+    if (!body.classList.contains('lock')) {
+      body.style.width = `${document.documentElement.clientWidth}px`;
+      body.classList.toggle('lock');
+    }
+  }
+
+  signIn_button.addEventListener('click', function (e) {
+    e.preventDefault();
+    showRegistration(form_signIn)
+  });
+
+  signUp_button.addEventListener('click', function (e) {
+    e.preventDefault();
+    showRegistration(form_signUp)
+  });
+
+  modal_close.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      e.preventDefault();
+      modal_register.classList.remove('showed');
+      form_signIn.classList.remove('showed');
+      form_signUp.classList.remove('showed');
+
+      if (!nav_menu.classList.contains('active')) {
+        body.classList.remove('lock');
+        body.style.width = `auto`;
+      }
+    });
+  });
+};
